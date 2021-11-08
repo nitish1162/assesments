76148cf4cfeb60e319dd9b5bf7e3dd9a91b4aafa
nitish1162 <nitishkc789@gmail.com>
2021-11-08 08:25:55 +0530

    adding assesment files

--- r.sh
+++ r.sh
@@ -1,4 +1,45 @@
-dice1=$((RANDOM%6))
-        dice2=$((RANDOM%6))
-        sum=$((dice1+dice2))
-        echo $sum
+MAXCOUNT=10
+count=1
+#Array name is number
+
+while [ "$count" -le $MAXCOUNT ]; do
+ number[$count]=$(( ( RANDOM % 10 ) + 100 ))
+ let "count += 1"
+done
+
+echo "${number[*]}"
+
+largest=${number[0]}
+secondGreatest='unset'
+
+for((i=1; i < ${#number[@]}; i++))
+do
+  if [[ ${number[i]} > $largest ]]
+  then
+    secondGreatest=$largest
+    largest=${number[i]}
+  elif (( ${number[i]} != $largest )) && { [[ "$secondGreatest" = "unset" ]] || [[ ${number[i]} > $secondGreatest ]]; }
+  then
+    secondGreatest=${number[i]}
+  fi
+done
+
+echo "secondGreatest = $secondGreatest"
+
+smallest=${number[0]}
+secondSmallest='unset'
+
+for((i=1; i < ${#number[@]}; i++))
+do
+  if [[ ${number[i]} < $smallest ]]
+  then
+    secondSmallest=$smallest
+    smallest=${number[i]}
+  elif (( ${number[i]} != $smallest )) && { [[ "$secondSmallest" = "unset" ]] || [[ ${number[i]} > $secondSmallest ]]; }
+  then
+    secondSmallest=${number[i]}
+  fi
+
+done
+echo "secondSmallest = $secondSmallest"
+
