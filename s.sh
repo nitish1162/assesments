135f404a79c8e715553e32871e84d654081c1344
nitish1162 <nitishkc789@gmail.com>
2021-11-08 09:07:56 +0530

    adding files

--- s.sh
+++ s.sh
@@ -1,34 +1,21 @@
-echo "enter an integer:"
-read input
-if [ $input -lt 1 ];then
-echo "not allowed!"
-exit 1
-fi
-# find factors and prime
+echo "Program to check if the number is Prime or not"
+echo "Enter number: "
 
-i=2
-count=0
+read number
+i=2 
 flag=0
-for ((i;i<$input;));do
-    
-  if [ `expr $input % $i` -eq 0 ];then
-      factor=$i
+while test $i -le `expr $number / 2`
+do
+if test `expr $number % $i` -eq 0
+then
+flag=1
+fi
 
-  for ((j=2;j<=`expr $factor / 2`;));do
-      flag=0
-      if [ `expr $factor % $j` -eq 0 ];then
-      flag=1
-      break
-      fi
-      j=`expr $j + 1`
-  done
-  if [ $flag -eq 0 ];then
-  echo "[ $factor ]"
-  count=1
-  fi
-  fi
-  i=`expr $i + 1`
-  done
-  if [ $count -eq 0 ];then
-  echo "no prime factors found except 1 and $input"
-  fi
+i=`expr $i + 1`
+done 
+if test $flag -eq 1
+then
+echo "The number is Not Prime"
+else
+echo "The number is Prime"
+fi
